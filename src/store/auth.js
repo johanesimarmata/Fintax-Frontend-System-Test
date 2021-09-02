b6942a53b0811fb25a743441d48087e382bd0a0c
Johanes M P Simarmata <jmpsimarmata@gmail.com>
2021-09-02 15:19:40 +0700

    Add email in auth vuex

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,19 +1,27 @@
 export default{
      namespaced: true,
      state: {
-          token: ''
+          token: '',
+          email: ''
      },
      mutations: {
           setToken: (state, payload) => {
                state.token = payload
+          },
+          setEmail: (state, payload) => {
+               state.email = payload
           }
      },
      actions: {
           setToken: ({commit}, payload) => {
                commit('setToken', payload)
+          },
+          setEmail: ({commit}, payload) => {
+               commit('setEmail', payload)
           }
      },
      getters: {
-          token: state => state.token
+          token: state => state.token,
+          email: state => state.email
      }
 }
