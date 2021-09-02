5d1b104038302134b167188d9653d36e49984213
Johanes M P Simarmata <jmpsimarmata@gmail.com>
2021-09-02 16:01:50 +0700

    Add Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container homeComponent p-3">
+    <h3 class="title">Hello, {{email ? email : 'there'}}!</h3>
+    <h6 class="subtitle text-muted">Here are informations that you can explore in this website:</h6>
+    <div class="row p-3">
+      <card-home
+        :infoWebsite="dataWebsite"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import CardHome from '../components/CardHome.vue'
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data(){
+    return{
+      dataWebsite: [
+        {title: 'Explore Users', description: 'You can explore users data that we fetch from REQRES Rest API', path: '/users'},
+        {title: 'Explore Employees', description: 'You can explore employees data such as name, age, and salary. You can use the table\'s filter ascending, descending, and the amount of entries.', path: '/employees'}
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    CardHome
+  },
+  computed:{
+    ...mapGetters({
+      email: 'auth/email'
+    })
   }
 }
 </script>
+
+<style scoped>
+  .title{
+    text-align: left;
+    letter-spacing: 2px;
+  }
+  .subtitle{
+    text-align: left;
+  }
+  .homeComponent{
+    background: #f8f9fa4f;
+    border-radius: 10px;
+  }
+
+  @media screen and (max-width:575px) {
+      .title, .subtitle{
+        text-align: center;
+      }
+  }
+</style>
