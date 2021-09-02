f6ca6c6bd4c883bd0f6ef52deec323dacc2b94b9
Johanes M P Simarmata <jmpsimarmata@gmail.com>
2021-09-02 15:47:27 +0700

    Add Loading Component

---
+++ src/components/Loading.vue
@@ -0,0 +1,13 @@
+<template>
+     <div class="d-flex flex-column">
+          <h2>Loading data...</h2>
+          <div class="spinner-border mt-2 mx-auto" role="status" aria-hidden="true"></div>
+     </div>
+</template>
+
+<script>
+export default {
+     name: 'Loading',
+
+}
+</script>
